1fada81d6c49e7ca14fc73458faf1987968373b7
Lambert Wixson <lambert@clarifai.com>
2018-10-17 13:50:31 -0400

    Jupyter notebook config

---
+++ .jupyter/custom/custom.js
@@ -0,0 +1,14 @@
+define([
+    'base/js/namespace',
+    'base/js/events'
+],
+       function(IPython, events) {
+	   events.on("notebook_loaded.Notebook",
+		     function () {
+			 // disable autosave
+			 IPython.notebook.set_autosave_interval(0); //in milliseconds
+		     }
+		    );
+	   //may include additional events.on() statements
+       }
+      );
